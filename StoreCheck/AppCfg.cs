1b2dd05fc186299578d649f2a5b13ea943ee5fcb
GeorgeMorozov <GeorgeMorozov@gmail.com>
2012-11-02 04:51:00 +0200

    Я создала новые справочники, чтобы  информация не вносилась с клавиатуры, а подтягивалась из справочника. Spr_TypeTA – справочник типов торговых агентов Spr_ChannelRetail- справочник каналов реализации
    В справочник Spr_CAP добавила поле «ТорговаяМарка» для группировки СКЮ. Также для группировки можно использовать поле «Ассортимент»
    (мы говорили про два варианта группировки кому и как будет удобнее).

---
+++ StoreCheck/AppCfg.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace StoreCheck
+{
+    public sealed class AppConfiguration : ConfigurationSection
+    {
+        [ConfigurationProperty("imagePath", IsRequired = true)]
+        public  string imagePath
+        {
+            get { return (string)base["imagePath"]; }
+            set { base["imagePath"] = value; }
+        }
+    }
+/*
+    private static AppConfiguration instance = null;
+
+    public static AppConfiguration Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (MailCenterConfiguration)WebConfigurationManager
+                     .GetSection("mailCenter");
+            }
+            return instance;
+        }
+    }
+*/	
+}
---
+++ StoreCheck/Models/Users.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreCheck.Models
+{
+     public enum enSortType
+    {
+         enSrtTM     = 0,         //По категории
+         enSrtAssort = 1,         //По имени
+            
+    };
+    
+    public partial class Users
+    {        
+        private enSortType _sortTp;
+        public Users()
+        {
+            
+        }
+
+        
+        public enSortType SortType
+        {
+            
+            get {
+                int sortBit = this.UserBits ?? 0;
+                if (Convert.ToBoolean(sortBit & (int)enSortType.enSrtAssort))
+                    _sortTp = enSortType.enSrtAssort;
+                else
+                    _sortTp = enSortType.enSrtTM;  
+                    return _sortTp;               
+                }
+            set {
+                    _sortTp = value;
+                    if (this.UserBits == null)
+                        this.UserBits = 0;
+                    if (_sortTp == enSortType.enSrtAssort)
+                        
+                        this.UserBits |= (int)enSortType.enSrtAssort;
+                    else
+                        this.UserBits &=~ (int)enSortType.enSrtAssort;
+                         
+                }
+        
+            /*
+            get
+            {
+                int sortBit = this.Sort;
+                if (Convert.ToBoolean(sortBit & (int)enSortType.enSrtAssort))
+                    _sortTp = enSortType.enSrtAssort;
+                else
+                    _sortTp = enSortType.enSrtTM;
+                return _sortTp;
+            }
+            set
+            {
+                _sortTp = value;
+                if (_sortTp == enSortType.enSrtAssort)
+
+                    this.Sort = 1;
+                else
+                    this.Sort = 0;
+
+            }
+             */ 
+        }
+    }
+}
